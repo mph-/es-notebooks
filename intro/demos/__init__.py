75ccc4c07bf247a4c19971aa316efbeb2eb2a543
Michael Hayes <michael.hayes@canterbury.ac.nz>
2022-03-13 20:42:58 +1300

    Add risetime demos

--- intro/demos/__init__.py
+++ intro/demos/__init__.py
@@ -6,3 +6,7 @@ from .IV_resistance_demo import *
 from .IV_resistance_demo2 import *
 from .IV_resistor_demo import *
 from .IV_thevenin_demo import *
+from .IV_open_drain_loading_demo import *
+from .risetime_demo1 import *
+from .risetime_demo2 import *
+from .risetime_demo3 import *
